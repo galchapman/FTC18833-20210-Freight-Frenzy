595e6d88e3db5841e66b1402c7a345c013e5847b
Megiddo Lions <megiddo.lions@gmail.com>
2022-02-14 12:45:56 +0200

    Some autos code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearRedAuto.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.annotation.SuppressLint;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.lib.StartingPosition;
+
+@Autonomous
+public class NearRedAuto extends LoadedAuto {
+    @SuppressLint("SdCardPath")
+    public NearRedAuto() {
+        super("/sdcard/FIRST/auto/near_red.auto",
+                "/sdcard/FIRST/trajectories/near_red.json",
+                new Pose2d(1.205, -1.63, Math.toRadians(90)),
+                StartingPosition.NearRed);
+    }
+}
