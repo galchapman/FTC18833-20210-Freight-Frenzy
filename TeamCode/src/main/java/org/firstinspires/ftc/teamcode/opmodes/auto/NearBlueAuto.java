8af5ca5fa5ec927887135738c7bb7141ed33cb7c
Megiddo Lions <megiddo.lions@gmail.com>
2022-03-10 10:33:38 +0200

    Add near blue auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/NearBlueAuto.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import android.annotation.SuppressLint;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.lib.StartingPosition;
+
+@Autonomous(name = "Near Blue", preselectTeleOp = "Blue Drive", group = "Auto: blue")
+public class NearBlueAuto extends LoadedAuto {
+    @SuppressLint("SdCardPath")
+    public NearBlueAuto() {
+        super("/sdcard/FIRST/auto/near_blue.auto",
+                "/sdcard/FIRST/trajectories/near_blue.json",
+                new Pose2d(-1.19, 1.63, Math.toRadians(90)),
+                StartingPosition.NearBlue);
+    }
+}
